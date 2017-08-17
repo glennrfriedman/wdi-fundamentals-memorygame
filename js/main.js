142d48bebf01397b330725df7605879f8c561b3d
Glenn Friedman <glennfriedman@Glenns-MBP.fios-router.home>
2017-08-17 13:16:06 -0400

    created game logic

---
+++ js/main.js
@@ -0,0 +1,13 @@
+ console.log("Up and running!");
+
+ var cardOne = "king";
+
+ var cardTwo = "king";
+
+ var cardThree = "queen";
+
+ var cardFour = "queen";
+
+console.log("User flipped " + cardFour)
+
+console.log("User flipped " + cardOne)
