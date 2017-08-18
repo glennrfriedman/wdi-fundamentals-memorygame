35d65318f845ec73d81314501a643132417021b4
Glenn Friedman <glennfriedman@Glenns-MBP.fios-router.home>
2017-08-18 13:27:00 -0400

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,13 +1,25 @@
- console.log("Up and running!");
+var cards = ["queen", "queen", "king", "king"];
 
- var cardOne = "king";
+var cardsInPlay = [];
 
- var cardTwo = "king";
+var cardOne = cards[0];
 
- var cardThree = "queen";
+cardsInPlay.push(cardOne);
 
- var cardFour = "queen";
+console.log("User flipped " + cardOne);
 
-console.log("User flipped " + cardFour)
+var cardTwo = cards[2];
 
-console.log("User flipped " + cardOne)
+cardsInPlay.push(cardTwo);
+
+console.log("User flipped " + cardTwo);
+
+if (cardsInPlay.length === 2) {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+    alert("You found a match!");
+  	} 
+  	else {
+    alert("Sorry, try again.");
+  	}
+}
+
